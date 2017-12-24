42271ffbab1f0eb3a8eb0eedc57fe747649f374a
zzxwill <zzxwill@gmail.com>
2017-12-25 00:02:06 +0800

    draft commit

--- templates/js/example.js
+++ templates/js/example.js
@@ -175,10 +175,10 @@ $(function () {
     function preload(){
         $(window).on("load", function(){
             var imgList = [
-                "./images/layers/content.png",
-                "./images/layers/navigation.png",
-                "./images/layers/popout.png",
-                "./images/layers/transparent.gif"
+                "../images/content.png",
+                "../images/navigation.png",
+                "../images/popout.png",
+                "../images/transparent.gif"
             ];
             for (var i = 0, len = imgList.length; i < len; ++i) {
                 new Image().src = imgList[i];
